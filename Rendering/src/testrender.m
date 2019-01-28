f82f7a451c1a3b4f19bcf0f18f61394e8a55d3e2
William-Boshell <william.boshell@colorado.edu>
2019-01-27 23:51:44 -0700

    Working renderer added. May add a little more functionality for convenince.

--- Rendering/src/testrender.m
+++ Rendering/src/testrender.m
@@ -7,48 +7,49 @@ f_1= imresize(imrotate(imread('C:\Users\Wil Boshell\mcm2019\Rendering\assets\_1.
 f_2 = imresize(imrotate(imread('C:\Users\Wil Boshell\mcm2019\Rendering\assets\_2.jpg'),-90),0.5);
 F={f_2,f_1,f0,f1,f2};
 
-ufile=inputdlg('Enter File Name:') %Will add path name through imports\. No .csv necessary.
-simfile=strcat('C:\Users\Wil Boshell\mcm2019\Rendering\imports\',ufile{1},'.csv');
 M=csvread('C:\Users\Wil Boshell\mcm2019\Rendering\exports\MasterNodes.csv');
-R=csvread(simfile);
+R=csvread('C:\Users\Wil Boshell\mcm2019\Rendering\imports\NaiveSetup.csv',15000-1,0);
 nt=size(R,1);
-RAM=1.2*10^10;
-dmin=ceil(850*1150*3*nt*5/RAM);
-d=str2num(cell2mat(inputdlg('Enter Step Size:')));
-ns=floor((nt-1)/d)+2;
-t(1)=1;
-for i=1:ns-2
-    t(i+1)=d*i;
-end
-t(ns)=nt;
-nf=5;
-
-I=zeros([850,1100,3,ns,nf],'uint8');
+%d=10;
+%ns=floor((nt-1)/d)+2;
+%t(1)=1;
+%for i=1:ns-2
+%     t(i+1)=d*i;
+% end
+% t(ns)=nt;
+%I=uint8(zeros(850,1100,3,ns));
 for l=1:5
     ib=F{l};
     N=[];
     for i=1:size(M,1)
-        if M(i,4)==l-3
+        if M(i,4)==l-3;
             N=[N;M(i,:)];
         end
-    end
-    nnodes=size(N,1);
-    for r = 1:nnodes
-        shape = reshape(untwist([N(r,8:11);N(r,12:15)]),[1,8]);
-        if sum(shape) == 0
-            ib = insertText(ib, [N(r,2) N(r,3)], ['E' num2str(N(r,1))],'AnchorPoint','Center','BoxOpacity',0);
-        else
-            ib = insertShape(ib, 'FilledPolygon', shape);
+        for r = 1:size(N,1)
+            shape = reshape(untwist([N(r,8:11);N(r,12:15)]),[1,8]);
+            if sum(shape) == 0
+                ib = insertText(ib, [N(r,2) N(r,3)], ['E' num2str(N(r,1))],'AnchorPoint','Center','BoxOpacity',0);
+            else
+                
+                ib = insertShape(ib, 'FilledPolygon', shape);
+                
+            end
         end
-    end
-    for i=1:ns
-        im=ib;
-        for r=1:nnodes
-            im = insertText(im, [mean(N(r,8:11)) mean(N(r,12:15))], num2str(R(t(i),r)),'AnchorPoint','Center','BoxOpacity',0);
-        end
-        I(:,:,:,i,l)=im;
-    end
+    end %23
+   
+        %ib=ib;
+        for r=1:size(N,1)
+            
+            ib = insertText(ib, [mean(N(r,8:11)) mean(N(r,12:15))], num2str(R(475-r)),'AnchorPoint','Center','BoxOpacity',0);
+            
+            end
+        %Mov(i)=im2frame(im);
+        %I(:,:,:,i)=ib;
+    figure
+    image(ib)
 end
+
+%image(im)
 
 
 function [shape] = untwist(shape)
